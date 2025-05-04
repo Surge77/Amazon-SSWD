f1678fb00a704547aadfc92cb5b8021cdd96347c
TEJAS <tejasdeshmane66@gmail.com>
2025-05-04 13:24:01 +0530

    save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,7 +1,7 @@
 import {cart,removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {formatCurrency } from '../utils/money.js';
-import { deliveryOptions }from '../../data/deliveryoptions.js'; 
+import { deliveryOptions, getDeliverOption }from '../../data/deliveryoptions.js'; 
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
 
@@ -14,23 +14,11 @@ export function renderOrderSummary(){
 
     let productId = cartItem.productId;
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-      if(product.id === productId){
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId  = cartItem.deliveryOptionId
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if(option.id === deliveryOptionId){
-        deliveryOption = option;
-      }
-    });
+    const deliveryOption = getDeliverOption(deliveryOptionId);
 
     const today = dayjs();
 
