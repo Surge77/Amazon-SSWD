b531787ffb61c34a7fce1144e5fff22fdd7d2e6b
TEJAS <tejasdeshmane66@gmail.com>
2025-05-05 15:35:46 +0530

    convert load Products from XHR to fetch

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -7,8 +7,8 @@ describe('test suite: renderOrderSummary', () => {
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
   beforeAll((done) => {
-      loadProducts(() => {
-        done();
+    loadProductsFetch().then(() => {
+      done();
     });
   });
 
@@ -66,4 +66,4 @@ describe('test suite: renderOrderSummary', () => {
 
     document.querySelector('.js-test-container').innerHTML = '';
   });
-}); 
+});
